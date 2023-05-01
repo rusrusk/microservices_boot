95691df00a5b123d8399896800c7d4acb25e4035
Ruslan Kultaev <ruslankultaev7@gmail.com>
2023-05-01 12:09:05 +0200

    added a method which returns a boolean to check if inventory is in stock. Now i need to load data to db to test functionality of controller

--- src/main/java/com/ruslank/springboot_microservices_inventory_app/SpringbootMicroservicesInventoryAppApplication.java
+++ src/main/java/com/ruslank/springboot_microservices_inventory_app/SpringbootMicroservicesInventoryAppApplication.java
@@ -1,13 +1,21 @@
 package com.ruslank.springboot_microservices_inventory_app;
 
+import com.ruslank.springboot_microservices_inventory_app.repositories.InventoryRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringbootMicroservicesInventoryAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootMicroservicesInventoryAppApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner loadData(InventoryRepository inventoryRepository) {
+
 	}
 
 }
---
+++ src/main/java/com/ruslank/springboot_microservices_inventory_app/controllers/InventoryController.java
@@ -0,0 +1,23 @@
+package com.ruslank.springboot_microservices_inventory_app.controllers;
+
+import com.ruslank.springboot_microservices_inventory_app.entities.Inventory;
+import com.ruslank.springboot_microservices_inventory_app.services.InventoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/inventory")
+@RequiredArgsConstructor
+public class InventoryController {
+
+    private final InventoryService inventoryService;
+
+    @GetMapping("inventories/{sku-code}")
+    @ResponseStatus(HttpStatus.OK)
+    public boolean isInventoryInStock(@PathVariable("sku-code") String skuCode) {
+        this.inventoryService.isInventoryInStock(skuCode);
+    }
+}

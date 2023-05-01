52c63b1cc6f046cf0e9ce2e181c2ee67f99f3043
Ruslan Kultaev <ruslankultaev7@gmail.com>
2023-05-01 09:44:35 +0200

    order service

---
+++ src/main/java/com/ruslank/springboot_microservices_order_app/SpringbootMicroservicesOrderAppApplication.java
@@ -0,0 +1,13 @@
+package com.ruslank.springboot_microservices_order_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMicroservicesOrderAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMicroservicesOrderAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ruslank/springboot_microservices_order_app/SpringbootMicroservicesOrderAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruslank.springboot_microservices_order_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMicroservicesOrderAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

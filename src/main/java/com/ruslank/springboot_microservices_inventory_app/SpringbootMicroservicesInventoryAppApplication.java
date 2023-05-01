6412e20462f7fedb621270e3de73526aecfaf749
Ruslan Kultaev <ruslankultaev7@gmail.com>
2023-05-01 11:48:38 +0200

    connecting mysql db

---
+++ src/main/java/com/ruslank/springboot_microservices_inventory_app/SpringbootMicroservicesInventoryAppApplication.java
@@ -0,0 +1,13 @@
+package com.ruslank.springboot_microservices_inventory_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMicroservicesInventoryAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMicroservicesInventoryAppApplication.class, args);
+	}
+
+}

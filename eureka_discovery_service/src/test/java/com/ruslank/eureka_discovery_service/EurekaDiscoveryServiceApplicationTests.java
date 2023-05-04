7fb9f366d8abcf999e89c2217de08e5db394f197
Ruslan Kultaev <ruslankultaev7@gmail.com>
2023-05-04 14:50:09 +0200

    containereizing services with jib maven plugin

---
+++ eureka_discovery_service/src/test/java/com/ruslank/eureka_discovery_service/EurekaDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruslank.eureka_discovery_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

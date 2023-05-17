af53cd1505524a925d3074b139cb3067abd75b88
Ruslan Kultaev <ruslankultaev7@gmail.com>
2023-05-17 14:52:50 +0200

    adding circuit breaker to order service

--- order_service/src/main/java/com/ruslank/springboot_microservices_order_app/controller/OrderController.java
+++ order_service/src/main/java/com/ruslank/springboot_microservices_order_app/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.ruslank.springboot_microservices_order_app.controller;
 import com.ruslank.springboot_microservices_order_app.dto.OrderRequest;
 import com.ruslank.springboot_microservices_order_app.entities.Order;
 import com.ruslank.springboot_microservices_order_app.services.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -16,8 +17,13 @@ public class OrderController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @CircuitBreaker(name="inventory", fallbackMethod = "fallBackMethod")
     public String createOrder(@RequestBody OrderRequest order) {
         this.orderService.createOrder(order);
         return "Order was created successfully";
+    }
+
+    public String fallBackMethod(OrderRequest orderRequest) {
+        return "The operation cannot be processed! Sorry!";
     }
 }
